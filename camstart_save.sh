54032acf00f0c7671e47afc83029140bf3623ab0
Colin Pate <pate.colin@gmail.com>
2020-12-08 06:41:50 +0000

    Changes for now

--- camstart_save.sh
+++ camstart_save.sh
@@ -2,7 +2,7 @@
 ./opencv/ar0330/ar0330
 ./opencv/remaptest/houghlines
 ./opencv/auto_exposure/ar0330
-./opencv/remapnowarp/houghlines
+#./opencv/remapnowarp/houghlines
 mount /dev/mmcblk0p1 /home/root/fbmount
-./opencv/save_imgs/houghlines
-umount /home/root/fbmount
+#./opencv/save_imgs/houghlines
+#umount /home/root/fbmount
